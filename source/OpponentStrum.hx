f7f92106aad313a785947c70180cca8632933395
Tahir <84905346+TahirArch@users.noreply.github.com>
2022-02-11 10:35:39 +0300

    Update OpponentStrum.hx

--- source/OpponentStrum.hx
+++ source/OpponentStrum.hx
@@ -34,7 +34,7 @@ class OpponentStrum extends FlxSprite
 		this.noteData = leData;
 		super(x, y);
 
-		var skin:String = 'NOTE_assets';
+		var skin:String = 'NOTE_circle';
 		if(PlayState.SONG.arrowSkin != null && PlayState.SONG.arrowSkin.length > 1) skin = PlayState.SONG.arrowSkin;
 		texture = skin; //Load texture and anims
 
